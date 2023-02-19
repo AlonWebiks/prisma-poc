8b8c76ebd8d0fec284cad0cfd09c6cebe1019992
AlonWebiks <alon@webiks.com>
2023-02-19 13:29:43 +0200

    remove dist

--- dist/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = exports.Prisma = void 0;
-var client_1 = require("@prisma/client");
-Object.defineProperty(exports, "Prisma", { enumerable: true, get: function () { return client_1.Prisma; } });
-const client_2 = require("@prisma/client");
-exports.prisma = new client_2.PrismaClient();
-//# sourceMappingURL=prisma.js.map
